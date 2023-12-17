a99c00adf68ae5637a1b3cb809d0a18349e4ba9c
terence <terenceshawn18@outlook.com>
2023-12-17 17:52:25 +0300

    finished customer.py

--- customer.py
+++ customer.py
@@ -1,1 +1,14 @@
+class Customer:
+    all_customers = []
+    def __init__(self, given_name, family_name):
+        # First name of the customer
+        self.given_name = given_name
+        # Last name of the customer
+        self.family_name = family_name
+        # List to store reviews written by this customer
+        self.reviews = []
+        Customer.all_customers.append(self)
 
+    # Method to return the full name of the customer
+    def full_name(self):
+        return f'{self.family_name} {self.given_name}'
